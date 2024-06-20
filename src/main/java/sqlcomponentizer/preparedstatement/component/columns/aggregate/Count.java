37972cfcda58f873e01c802b85398a3b5443e5c7
Alex C <acoundou@gmail.com>
2024-06-20 00:19:13 -0400

    Added distinct keyword in Count

--- src/main/java/sqlcomponentizer/preparedstatement/component/columns/aggregate/Count.java
+++ src/main/java/sqlcomponentizer/preparedstatement/component/columns/aggregate/Count.java
@@ -5,13 +5,22 @@ import sqlcomponentizer.preparedstatement.utility.ParenthesizedString;
 public class Count extends ParenthesizedString {
 
     private final String action = "COUNT";
+    private final String distinctString = "DISTINCT";
 
-    public Count(String column) {
+    private Boolean distinct;
+
+    public Count(String column, Boolean distinct) {
         super(column);
+        this.distinct = distinct;
     }
 
     @Override
     public String toString() {
-        return action + super.toString();
+        // TODO: This is less concise than it was previously, is there a way to make it more concise?
+        if (distinct) {
+            return distinctString + action + super.toString();
+        } else {
+            return action + super.toString();
+        }
     }
 }
